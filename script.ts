dd6d31af328c59cba088a6a714d00438818fe40c
akash dudhal <akashdudhal62@gmail.com>
2024-09-30 17:19:48 +0530

    Initial commit added: class added with method add, prints input as it is

---
+++ script.ts
@@ -0,0 +1,9 @@
+class IncuByteAssesment {
+
+    public add(input: string) {
+        return input;
+    }
+}
+
+const instance = new IncuByteAssesment();
+console.log("Addition:", instance.add(""));
