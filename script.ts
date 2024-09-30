b570743335140a151409ebee916807d9009dd2a2
akash dudhal <akashdudhal62@gmail.com>
2024-09-30 17:29:45 +0530

    Test case 1: Return 0 if input is empty string

--- script.ts
+++ script.ts
@@ -1,9 +1,12 @@
-class IncuByteAssesment {
-
-    public add(input: string) {
+class IncubyteAssignment {
+    public add(input: string): any {
+        // Test case 1:  Input: “”, Output: 0
+        if (!input) {
+            return 0;
+        }
         return input;
     }
 }
 
-const instance = new IncuByteAssesment();
-console.log("Addition:", instance.add(""));
+const instance = new IncubyteAssignment();
+console.log('Add:', instance.add(''));
